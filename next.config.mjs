2e006abdc5f7acd0e54a735e4a9acb96b68b327b
Tiboss-Art <digihouse10@gmail.com>
2026-09-15 23:33:13 +0000

    second builds

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,13 @@ const nextConfig = {
     ],
     unoptimized: true,
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': new URL('./src', import.meta.url).pathname,
+    };
+    return config;
+  },
   async headers() {
     return [
       {
